e1478e4faa74d4cf5bb0d32445737ae2dce8d2c5
pokrasa <pawelokrasa198@gmail.com>
2019-12-25 21:03:22 +0100

    Some home work

--- lab2/src/main/scala/Lab02.scala
+++ lab2/src/main/scala/Lab02.scala
@@ -68,14 +68,38 @@ object Lab02 extends App {
 
   // Ćwiczenie 5
   // @annotation.tailrec
-  def filter[A](x: List[A], f: A => Boolean): List[A] = ???
+  def filter[A](x: List[A], f: A => Boolean): List[A] = {
+    @annotation.tailrec
+    def filterAcc[A](list:List[A], f: A => Boolean, result:List[A]) : List[A] = list match {
+      case List() => result
+      case head::tail => head match {
+        case x if f(x) => filterAcc(tail, f, result :+ head)
+        case _ => filterAcc(tail, f, result)
+      }        
+    }
+    filterAcc(x, f, List[A]())
+  }
+
+  println(filter(List(1,2,3,4), (x:Int) => x > 2))
 
   // Ćwiczenie 6
   // funkcja "cleanup" powinna usuwać wielokrotne, następujące po sobie
   // wystąpienia tego samego elementu na liście
   // np. cleanup(List(1,1,2,1,3,3)) == List(1,2,1,3)
   // @annotation.tailrec
-  def cleanup(x: List[Int]): List[Int] = ???
+  def cleanup(x: List[Int]): List[Int] = {
+    @annotation.tailrec
+    def cleanupAcc(list: List[Int], result:List[Int], known:List[Int]) : List[Int] = list match {
+      case List() => result
+      case head::tail => head match {
+        case x if known.contains(head) => cleanupAcc(tail, result, known)
+        case _ => cleanupAcc(tail, result :+ head, known :+ head)
+      }
+    }
+    cleanupAcc(x, List(), List())
+  }
+
+  println(cleanup(List(1,2,2,2,3,4,2,4,2,5)))
 
   // Ćwiczenie 7
   // funkcja "chop" powinna wycinać „podlistę” zaczynającą się od elementu
@@ -83,19 +107,52 @@ object Lab02 extends App {
   // pierwszy element listy ma numer 1. Przykład
   // chop(List('a,2,'b,3,'c,4),2,4) == List(2,'b,3)
   // @annotation.tailrec
-  def chop[A](x: List[A], b: Int, e: Int): List[A] = ???
+  def chop[A](x: List[A], b: Int, e: Int): List[A] = {
+    @annotation.tailrec
+    def chopAcc(list: List[A], b: Int, e: Int, result :List[A] = List(), iter : Int = 0) : List[A] = list match {
+      case List() => result
+      case x if iter > e => result
+      case head::tail if iter >= b => chopAcc(tail, b, e, result :+ head, iter + 1)
+      case head::tail => chopAcc(tail, b, e, result, iter + 1)
+    }
+    chopAcc(x, b, e)
+  }
+
+  println(chop(List(1,2,3,4,5,6), 2, 10))
 
   // Ćwiczenie 8
   // funkcja "remEls" powinna usuwać co "k"-ty element listy. Przykład:
   // remEls(List(1,1,2,1,3,3),3) == List(1,1,1,3)
   // @annotation.tailrec
-  def remEls[A](x: List[A], k: Int): List[A] = ???
+  def remEls[A](x: List[A], k: Int): List[A] = {
+    @annotation.tailrec
+    def remElsAcc(list:List[A], interval : Int, result :List[A] = List(), counter : Int = 1) : List[A] = list match {
+      case List() => result
+      case head::tail if counter == interval => remElsAcc(tail, interval, result, 1)
+      case head::tail => remElsAcc(tail, interval, result :+ head, counter + 1)
+    }
+    remElsAcc(x, k)
+  }
+
+  println(remEls(List(1,2,3,4,5,6,7), 2))
 
   // Ćwiczenie 9
   // funkcja "rot" powinna przesuwać cyklicznie elementy listy o wartość "k".
   // Przykład:
   // rot(List(1,2,3,4,5,6),3) == List(4,5,6,1,2,3)
   // @annotation.tailrec
-  def rot[A](x: List[A], k: Int): List[A] = ???
+  def rot[A](x: List[A], k: Int): List[A] = {
+    @annotation.tailrec
+    def rotAcc(list: List[A], shift: Int, result: List[A] = List(), temp: List[A] = List(), counter : Int = 0) : List[A] = list match {
+      case List() => temp match {
+        case List() => result
+        case head::tail => rotAcc(list, shift, result :+ head, tail, counter)
+      }
+      case head::tail if counter < shift => rotAcc(tail, shift, result, temp :+ head, counter + 1)
+      case head::tail if counter >= shift => rotAcc(tail, shift, result :+ head, temp, counter)
+    }
+    rotAcc(x,k)
+  }
 
+  println(rot(List(1,2,3,4,5,6),3))
 }
